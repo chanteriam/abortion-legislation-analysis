c3a70c32dcccb80d56d75ac56d11446636bf485c
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-02-19 13:28:05 -0600

    Update service.py

--- legislation_analysis/processing/service.py
+++ legislation_analysis/processing/service.py
@@ -24,7 +24,7 @@ def run_data_cleaner() -> None:
     scotus_cleaner = Cleaner(SCOTUS_DATA_FILE, "scotus_cases_cleaned.fea")
 
     # Clean congressional legislation
-    logging.debug("Cleaning Congress Data...")
+    logging.info("Cleaning Congress Data...")
     congress_cleaner.process(
         cols_to_clean=[
             ("raw_text", "cleaned_text"),
@@ -34,7 +34,7 @@ def run_data_cleaner() -> None:
     save(congress_cleaner.cleaned_df, congress_cleaner.save_path)
 
     # Clean SCOTUS opinions
-    logging.debug("Cleaning SCOTUS Data...")
+    logging.info("Cleaning SCOTUS Data...")
     scotus_cleaner.process()
     save(scotus_cleaner.cleaned_df, scotus_cleaner.save_path)
 
@@ -51,7 +51,7 @@ def run_data_tokenizer() -> None:
     )
 
     # Tokenize congressional legislation
-    logging.debug("Tokenizing Congress Data...")
+    logging.info("Tokenizing Congress Data...")
     congress_tokenizer.process(
         cols_to_tokenize=[
             ("cleaned_text", "tokenized_text"),
@@ -61,7 +61,7 @@ def run_data_tokenizer() -> None:
     save(congress_tokenizer.tokenized_df, congress_tokenizer.save_path)
 
     # Tokenize SCOTUS opinions
-    logging.debug("Tokenizing SCOTUS Data...")
+    logging.info("Tokenizing SCOTUS Data...")
     scotus_tokenizer.process()
     save(scotus_tokenizer.tokenized_df, scotus_tokenizer.save_path)
 
@@ -86,7 +86,7 @@ def run_pos_tagger() -> None:
     )
 
     # Apply POS tagging to congressional legislation
-    logging.debug("Applying POS tagging to congressional text...")
+    logging.info("Applying POS tagging to congressional text...")
     congress_pos.process(
         cols_to_apply_pos=[
             ("cleaned_text", "text_pos"),
@@ -97,6 +97,6 @@ def run_pos_tagger() -> None:
     save(congress_pos.pos_df, congress_pos.save_path)
 
     # Apply POS tagging to SCOTUS opinions
-    logging.debug("Applying POS tagging to SCOTUS text...")
+    logging.info("Applying POS tagging to SCOTUS text...")
     scotus_pos.process(tags_of_interest=tags_of_interest)
     save(scotus_pos.pos_df, scotus_pos.save_path)
