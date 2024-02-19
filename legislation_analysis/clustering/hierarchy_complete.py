54920933cb48e8e4ebb70c0e7d7ba900b2d2383a
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-02-19 15:38:39 -0600

    Update hierarchy_complete.py

--- legislation_analysis/clustering/hierarchy_complete.py
+++ legislation_analysis/clustering/hierarchy_complete.py
@@ -10,6 +10,8 @@ from legislation_analysis.clustering.abstract_clustering import (
 )
 from legislation_analysis.utils.constants import (
     CLUSTERED_DATA_PATH,
+    OPTIMAL_CONGRESS_CLUSTERS,
+    OPTIMAL_SCOTUS_CLUSTERS,
     TAGS_OF_INTEREST,
 )
 from legislation_analysis.utils.functions import (
@@ -27,15 +29,14 @@ class HierarchyComplete(AbstractClustering):
         self,
         file_path: str,
         file_name: str,
-        n_clusters: int = 32,
     ):
         self._df = load_file_to_df(file_path)
-        self._n_clusters = n_clusters
-        self._title_suffix = (
-            "SCOTUS Decisions"
-            if "scotus" in file_name
-            else "Congressional Legislation"
-        )
+        if "congress" in file_name:
+            self._n_clusters = OPTIMAL_CONGRESS_CLUSTERS
+            self._title_suffix = "Congressional Legislation"
+        else:
+            self._n_clusters = OPTIMAL_SCOTUS_CLUSTERS
+            self._title_suffix = "SCOTUS Decisions"
         self._save_path = os.path.join(CLUSTERED_DATA_PATH, file_name)
         # This vectorizer is configured so that a word cannot show up in more
         # than half the documents, must show up at least 3x, and the model can
@@ -65,6 +66,7 @@ class HierarchyComplete(AbstractClustering):
             cluster_algo = scipy.cluster.hierarchy.fcluster(
                 linkage_matrix, self._n_clusters, "maxclust"
             )
+
             self._df[f"{tag}_hc_clusters"] = cluster_algo
             logging.debug(f"Finished Hierarchy Complete clustering for {tag}.")
         logging.debug("Saving Hierarchy Complete assignments.")
