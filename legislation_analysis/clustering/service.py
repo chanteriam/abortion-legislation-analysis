9b23bc206cddd5f7cc66d4ee464edcdff51c083b
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-02-19 11:45:55 -0600

    Update service.py

--- legislation_analysis/clustering/service.py
+++ legislation_analysis/clustering/service.py
@@ -1,10 +1,19 @@
 def run_hierarchy_complete_clustering() -> None:
+    """
+    Runs Hierarchy Complete clustering.
+    """
     pass
 
 
 def run_hierarchy_ward_clustering() -> None:
+    """
+    Runs Hierarchy Ward clustering.
+    """
     pass
 
 
 def run_knn_clustering() -> None:
+    """
+    Runs K-Nearest Neighbor clustering.
+    """
     pass
