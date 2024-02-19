d8dc9a4dcbc20b23f9935648d0cc9fd101f5210b
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-02-18 23:17:22 -0600

    init methods

--- legislation_analysis/clustering/knn.py
+++ legislation_analysis/clustering/knn.py
@@ -11,19 +11,22 @@ class KNN(AbstractClustering):
     Class for implementing k-nearest neighbor clustering.
     """
 
-    def __init__(self, df: pd.DataFrame, n_clusters=32):
-        self.df = df
+    def __init__(
+        self, df: pd.DataFrame, df_column: str = "cleaned_text", n_clusters=32
+    ):
+        self._df = df
         self.n_cluster = n_clusters
         # This vectorizer is configured so that a word cannot show up in more
         # than half the documents, must show up at least 3x, and the model can
         # only have a maximum of 1000 features.
-        self.tfidf_vectorizer = sklearn.feature_extraction.text.TfidfVectorizer(
+        self._vectorizer = sklearn.feature_extraction.text.TfidfVectorizer(
             max_df=0.5,
             max_features=1000,
             min_df=3,
             stop_words="english",
             norm="l2",
         )
+        self._vectors = self._vectorizer.fit_transform(self._df[df_column])
 
     def execute(self):
         pass
