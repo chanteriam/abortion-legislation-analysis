bbdce07718bd4256af75fa7ea6298ff8c038b492
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-02-19 11:50:28 -0600

    Update service.py

--- legislation_analysis/clustering/service.py
+++ legislation_analysis/clustering/service.py
@@ -1,19 +1,25 @@
+import logging
+
+
 def run_hierarchy_complete_clustering() -> None:
     """
     Runs Hierarchy Complete clustering.
     """
-    pass
+    logging.info("Starting Hierarchy Complete clustering...")
+    logging.info("Finished Hierarchy Complete clustering...")
 
 
 def run_hierarchy_ward_clustering() -> None:
     """
     Runs Hierarchy Ward clustering.
     """
-    pass
+    logging.info("Starting Hierarchy Ward clustering...")
+    logging.info("Finished Hierarchy Ward clustering...")
 
 
 def run_knn_clustering() -> None:
     """
     Runs K-Nearest Neighbor clustering.
     """
-    pass
+    logging.info("Starting K-Nearest Neighbor clustering...")
+    logging.info("Finished K-Nearest Neighbor clustering...")
