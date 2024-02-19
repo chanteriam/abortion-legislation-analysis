610077420bc182fbecebbd76ac8ad48dd6d37052
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-02-18 22:52:57 -0600

    Update abstract_clustering.py

--- legislation_analysis/clustering/abstract_clustering.py
+++ legislation_analysis/clustering/abstract_clustering.py
@@ -1,4 +1,7 @@
+import logging
 from abc import ABC, abstractmethod
+
+import sklearn
 
 
 class AbstractClustering(ABC):
@@ -7,9 +10,29 @@ class AbstractClustering(ABC):
     """
 
     @abstractmethod
-    def _execute(self):
+    def execute(self):
         pass
 
     @abstractmethod
-    def execute(self):
+    def visualize(self):
         pass
+
+    @staticmethod
+    def cluster_scoring(df_column: str, labels):
+        logging.info("Cluster scoring:")
+        logging.info(
+            f"\tHomogeneity: "
+            f"{sklearn.metrics.homogeneity_score(df_column, labels):0.3f}"
+        )
+        logging.info(
+            f"\tCompleteness: "
+            f"{sklearn.metrics.completeness_score(df_column, labels):0.3f}"
+        )
+        logging.info(
+            f"\tV-measure: "
+            f"{sklearn.metrics.v_measure_score(df_column, labels):0.3f}"
+        )
+        logging.info(
+            "\tAdjusted Rand Score: "
+            f"{sklearn.metrics.adjusted_rand_score(df_column, labels):0.3f}"
+        )
