6e2e08a330f768e95c10df82d0f11f8aa0e4a4d9
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-02-19 15:24:31 -0600

    Update hierarchy_complete.py

--- legislation_analysis/clustering/hierarchy_complete.py
+++ legislation_analysis/clustering/hierarchy_complete.py
@@ -1,11 +1,20 @@
+import logging
+import os
+
 import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
 import scipy
 import sklearn
 
 from legislation_analysis.clustering.abstract_clustering import (
     AbstractClustering,
+)
+from legislation_analysis.utils.constants import (
+    CLUSTERED_DATA_PATH,
+    TAGS_OF_INTEREST,
+)
+from legislation_analysis.utils.functions import (
+    load_file_to_df,
+    save_df_to_file,
 )
 
 
@@ -16,16 +25,18 @@ class HierarchyComplete(AbstractClustering):
 
     def __init__(
         self,
-        df: pd.DataFrame,
-        df_column: str = "cleaned_text",
+        file_path: str,
+        file_name: str,
         n_clusters: int = 32,
-        is_scotus: bool = False,
     ):
-        self._df = df
-        self._title_suffix = (
-            "SCOTUS Decisions" if is_scotus else "Congressional Legislation"
-        )
+        self._df = load_file_to_df(file_path)
         self._n_clusters = n_clusters
+        self._title_suffix = (
+            "SCOTUS Decisions"
+            if "scotus" in file_name
+            else "Congressional Legislation"
+        )
+        self._save_path = os.path.join(CLUSTERED_DATA_PATH, file_name)
         # This vectorizer is configured so that a word cannot show up in more
         # than half the documents, must show up at least 3x, and the model can
         # only have a maximum of 1000 features.
@@ -36,29 +47,47 @@ class HierarchyComplete(AbstractClustering):
             stop_words="english",
             norm="l2",
         )
-        self._vectors = self._vectorizer.fit_transform(self._df[df_column])
 
-        self._vectors.todense()
-        vector_matrix = self._vectors * self._vectors.T
-        vector_matrix.setdiag(0)
+    def cluster_parts_of_speech(self) -> None:
+        for tag in TAGS_OF_INTEREST:
+            logging.debug(f"Starting Hierarchy Complete clustering for {tag}.")
+            vectors = self._vectorizer.fit_transform(
+                self._df[f"{tag}_tags_of_interest"]
+            )
 
-        self._linkage_matrix = scipy.cluster.hierarchy.complete(
-            vector_matrix.toarray()
-        )
-        self._cluster_algo = scipy.cluster.hierarchy.fcluster(
-            self._linkage_matrix, self._n_clusters, "maxclust"
-        )
+            vectors.todense()
+            vector_matrix = vectors * vectors.T
+            vector_matrix.setdiag(0)
 
-    def get_labels(self) -> np.ndarray:
-        return self._cluster_algo
+            linkage_matrix = scipy.cluster.hierarchy.complete(
+                vector_matrix.toarray()
+            )
+            cluster_algo = scipy.cluster.hierarchy.fcluster(
+                linkage_matrix, self._n_clusters, "maxclust"
+            )
+            self._df[f"{tag}_hc_clusters"] = cluster_algo
+            logging.debug(f"Finished Hierarchy Complete clustering for {tag}.")
+        logging.debug("Saving Hierarchy Complete assignments.")
+        save_df_to_file(self._df, self._save_path)
 
-    def visualize(self) -> None:
+    def visualize(self, tag: str) -> None:
         plt.title(
             "Hierarchical Complete Clustering Dendrogram "
             f"of {self._title_suffix}"
         )
         plt.xlabel("Cluster Size")
+        vectors = self._vectorizer.fit_transform(
+            self._df[f"{tag}_tags_of_interest"]
+        )
+
+        vectors.todense()
+        vector_matrix = vectors * vectors.T
+        vector_matrix.setdiag(0)
+
+        linkage_matrix = scipy.cluster.hierarchy.complete(
+            vector_matrix.toarray()
+        )
         scipy.cluster.hierarchy.dendrogram(
-            self._linkage_matrix, p=5, truncate_mode="level"
+            linkage_matrix, p=5, truncate_mode="level"
         )
         plt.show()
