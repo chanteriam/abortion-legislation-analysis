31415b5ffa733d6a671a180d57932a7fca4b8100
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-02-19 22:09:48 -0600

    Update constants.py

--- legislation_analysis/utils/constants.py
+++ legislation_analysis/utils/constants.py
@@ -1,7 +1,7 @@
 import os
 
 
-# Base Path Constants
+# base paths
 PROJECT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 API_DATA_PATH = os.path.join(PROJECT_PATH, "data", "api")
 CLEANED_DATA_PATH = os.path.join(PROJECT_PATH, "data", "cleaned")
@@ -9,7 +9,7 @@ CLUSTERED_DATA_PATH = os.path.join(PROJECT_PATH, "data", "clustered")
 PROCESSED_DATA_PATH = os.path.join(PROJECT_PATH, "data", "processed")
 RAW_DATA_PATH = os.path.join(PROJECT_PATH, "data", "raw")
 
-# API Constants
+# api
 CONGRESS_API_KEY = os.environ.get("CONGRESS_API_KEY")
 CONGRESS_API_ROOT_URL = "https://api.congress.gov/v3/bill/"
 CONGRESS_ROOT_URL = "https://congress.gov/"
@@ -20,7 +20,7 @@ SCOTUS_DATA_URL = (
 )
 
 
-# Data File Constants
+# data files
 CONGRESS_DATA_FILE = os.path.join(
     API_DATA_PATH, "congress_abortion_legislation_full-text.csv"
 )
@@ -32,7 +32,7 @@ SCOTUS_DATA_FILE_CLEANED = os.path.join(
     CLEANED_DATA_PATH, "scotus_cases_cleaned.fea"
 )
 
-# General Constants
+# general
 # Cluster numbers determined in exercise 3
 OPTIMAL_CONGRESS_CLUSTERS = 32
 OPTIMAL_SCOTUS_CLUSTERS = 6
