cbf10f6d3accb30c09954f5faab69c4e4c01d082
michplunkett <5885605+michplunkett@users.noreply.github.com>
2024-02-19 22:28:29 -0600

    Update service.py

--- legislation_analysis/clustering/service.py
+++ legislation_analysis/clustering/service.py
@@ -1,20 +1,13 @@
 import logging
-import os
 
 from legislation_analysis.clustering.hierarchy_complete import HierarchyComplete
 from legislation_analysis.clustering.hierarchy_ward import HierarchyWard
 from legislation_analysis.clustering.knn import KNN
-from legislation_analysis.utils.constants import PROCESSED_DATA_PATH
-
-
-CONGRESS_CLUSTERED_FILE = "congress_legislation_clustered.fea"
-CONGRESS_POS_TAGGED_FILE = os.path.join(
-    PROCESSED_DATA_PATH, "congress_legislation_pos.fea"
-)
-
-SCOTUS_CLUSTERED_FILE = "scotus_cases_clustered.fea"
-SCOTUS_POS_TAGGED_FILE = os.path.join(
-    PROCESSED_DATA_PATH, "scotus_cases_pos.fea"
+from legislation_analysis.utils.constants import (
+    CONGRESS_DATA_CLUSTERED_FILE_NAME,
+    CONGRESS_DATA_POS_TAGGED_FILE,
+    SCOTUS_DATA_FILE_CLUSTERED_NAME,
+    SCOTUS_DATA_FILE_POS_TAGGED,
 )
 
 
@@ -27,7 +20,7 @@ def run_hierarchy_complete_clustering() -> None:
         "Congressional legislation..."
     )
     congress_hc = HierarchyComplete(
-        CONGRESS_POS_TAGGED_FILE, CONGRESS_CLUSTERED_FILE
+        CONGRESS_DATA_POS_TAGGED_FILE, CONGRESS_DATA_CLUSTERED_FILE_NAME
     )
     congress_hc.cluster_parts_of_speech()
     logging.info(
@@ -38,7 +31,9 @@ def run_hierarchy_complete_clustering() -> None:
     logging.info(
         "Starting Hierarchy Complete clustering for SCOTUS decisions..."
     )
-    scotus_hc = HierarchyComplete(SCOTUS_POS_TAGGED_FILE, SCOTUS_CLUSTERED_FILE)
+    scotus_hc = HierarchyComplete(
+        SCOTUS_DATA_FILE_POS_TAGGED, SCOTUS_DATA_FILE_CLUSTERED_NAME
+    )
     scotus_hc.cluster_parts_of_speech()
     logging.info(
         "Finished Hierarchy Complete clustering for SCOTUS decisions..."
@@ -54,7 +49,7 @@ def run_hierarchy_ward_clustering() -> None:
         "Starting Hierarchy Ward clustering for Congressional legislation..."
     )
     congress_hw = HierarchyWard(
-        CONGRESS_POS_TAGGED_FILE, CONGRESS_CLUSTERED_FILE
+        CONGRESS_DATA_POS_TAGGED_FILE, CONGRESS_DATA_CLUSTERED_FILE_NAME
     )
     congress_hw.cluster_parts_of_speech()
     logging.info(
@@ -62,7 +57,9 @@ def run_hierarchy_ward_clustering() -> None:
     )
 
     logging.info("Starting Hierarchy Ward clustering for SCOTUS decisions...")
-    scotus_hw = HierarchyWard(SCOTUS_POS_TAGGED_FILE, SCOTUS_CLUSTERED_FILE)
+    scotus_hw = HierarchyWard(
+        SCOTUS_DATA_FILE_POS_TAGGED, SCOTUS_DATA_FILE_CLUSTERED_NAME
+    )
     scotus_hw.cluster_parts_of_speech()
     logging.info("Finished Hierarchy Ward clustering for SCOTUS decisions...")
 
@@ -75,7 +72,9 @@ def run_knn_clustering() -> None:
         "Starting K-Nearest Neighbor clustering for "
         "Congressional legislation..."
     )
-    congress_knn = KNN(CONGRESS_POS_TAGGED_FILE, CONGRESS_CLUSTERED_FILE)
+    congress_knn = KNN(
+        CONGRESS_DATA_POS_TAGGED_FILE, CONGRESS_DATA_CLUSTERED_FILE_NAME
+    )
     congress_knn.cluster_parts_of_speech()
     logging.info(
         "Finished K-Nearest Neighbor clustering for "
@@ -85,7 +84,9 @@ def run_knn_clustering() -> None:
     logging.info(
         "Starting K-Nearest Neighbor clustering for SCOTUS decisions..."
     )
-    scotus_knn = KNN(SCOTUS_POS_TAGGED_FILE, SCOTUS_CLUSTERED_FILE)
+    scotus_knn = KNN(
+        SCOTUS_DATA_FILE_POS_TAGGED, SCOTUS_DATA_FILE_CLUSTERED_NAME
+    )
     scotus_knn.cluster_parts_of_speech()
     logging.info(
         "Finished K-Nearest Neighbor clustering for SCOTUS decisions..."
